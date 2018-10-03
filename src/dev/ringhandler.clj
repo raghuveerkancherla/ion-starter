79ff39624e659446652e0d2f1bd90607f1b65f0e
Raghu <raghu@aplopio.com>
2018-10-03 16:46:04 +0530

    created a generic web request ion handler

--- src/dev/ringhandler.clj
+++ src/dev/ringhandler.clj
@@ -1,9 +1,9 @@
 (ns dev.ringhandler
   (:require [org.httpkit.server :as httpkit]
-            [datomic.ion.starter :as app]))
+            [ion-starter.core :as app]))
 
 (defn handler [request]
-  (app/items-by-type-web* request))
+  (app/web-request-handler-ion* request))
 
 (defn run-handler [port]
   (httpkit/run-server handler {:port port}))
---
+++ src/ion_starter/core.clj
@@ -0,0 +1,35 @@
+(ns ion-starter.core
+  (:require
+    [clojure.data.json :as json]
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]
+    [clojure.pprint :as pp]
+    [datomic.client.api :as d]
+    [datomic.ion.lambda.api-gateway :as apigw]))
+
+(def get-client
+  "This function will return a local implementation of the client
+interface when run on a Datomic compute node. If you want to call
+locally, fill in the correct values in the map."
+  (memoize #(d/client {:server-type :ion
+                       :region      "us-east-1"
+                       :system      "myapp"
+                       ;:query-group "stu-8"
+                       :endpoint    "http://entry.myapp.us-east-1.datomic.net:8182/"
+                       :proxy-port  8182})))
+
+(defn web-request-handler-ion*
+  "Lambda ion that returns sample database items matching type."
+  [{:keys [headers body] :as request}]
+
+    (if (= (:uri request) "/api")
+      {:status 200
+       :headers {"Content-Type" "text/html"}
+       :body "Got a api request"}
+      {:status 200
+       :headers {"Content-Type" "text/html"}
+       :body "Got a request to serve index.html"}))
+
+(def web-request-handler-ion
+  "API Gateway web service ion for items-by-type"
+  (apigw/ionize web-request-handler-ion*))
